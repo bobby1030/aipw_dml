8b121d49ec28703ee4b1488d513b4e8f8f0629ca
Bobby Ho <angela.bobby@gmail.com>
2023-05-15 14:22:34 +0800

    ate: aipw estimator

--- estimation.R
+++ estimation.R
@@ -24,3 +24,37 @@ ate_ipw <- function(data, formula_pscore) {
         )
     )
 }
+
+ate_aipw <- function(data, formula_pscore, formula_response) {
+    # estimate propensity score using LOESS
+    pscore_fit <- loess(formula_pscore, data = data)$fitted
+    data$pscore_fit <- pscore_fit
+
+    # estimate response function using LOESS
+    response_model_treat <- loess(formula_response, data = data[data$D == 1, ])
+    response_model_contr <- loess(formula_response, data = data[data$D == 0, ])
+
+    data$response_fit_treat <- predict(response_model_treat, data)
+    data$response_fit_contr <- predict(response_model_contr, data)
+
+    # estimate ATE using AIPW
+    ate_aipw <- data %>%
+        drop_na(pscore_fit, response_fit_treat, response_fit_contr) %>%
+        mutate(
+            weight = ifelse(D == 1, 1 / pscore_fit, 1 / (1 - pscore_fit)),
+            scale = (D - pscore_fit) / (pscore_fit * (1 - pscore_fit)),
+            weighted_response = (1 - pscore_fit) * response_fit_treat + pscore_fit * response_fit_contr
+        ) %>%
+        summarize(
+            ate_aipw = (sum(weight * D * Y) - sum(weight * (1 - D) * Y) - sum(scale * weighted_response)) / n()
+        )
+
+    return(
+        list(
+            ate = ate_aipw,
+            pscore_fit = pscore_fit,
+            response_fit_treat = data$response_fit_treat,
+            response_fit_contr = data$response_fit_contr
+        )
+    )
+}
