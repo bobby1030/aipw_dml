1a1b331a8f81a32b4ee967448fd9c18799953d75
Bobby Ho <angela.bobby@gmail.com>
2023-05-20 01:30:09 +0800

    graph: helper functions in plotting

--- graph.R
+++ graph.R
@@ -1,16 +1,29 @@
 library(tidyverse)
 
-simulation_result <- readRDS("./simulation_result.rds")
+simulation_result <- readRDS("./simulation_result_lasso.rds")
 
-simulation_result %>% 
+simulation_result %>%
     pivot_longer(everything(), names_to = "estimator", values_to = "ate") %>%
     ggplot(aes(x = ate, color = estimator)) +
-        geom_density() +
-        geom_point(
-            aes(y = 0),
-            data = . %>% 
-                group_by(estimator) %>%
-                summarize(ate = mean(ate)),
-        ) +
-        geom_vline(xintercept = 5, linetype = "dashed") +
-        theme_minimal()
+    geom_density() +
+    geom_point(
+        aes(y = 0),
+        data = . %>%
+            group_by(estimator) %>%
+            summarize(ate = mean(ate)),
+    ) +
+    geom_vline(xintercept = 5, linetype = "dashed") +
+    theme_minimal()
+
+
+plot(density(data$pscore[data$D == 0]), xlim = c(0, 1))
+lines(density(data$pscore[data$D == 1]), xlim = c(0, 1))
+
+lines(density(forest$W.hat[data$D == 0]), xlim = c(0, 1), col = "red")
+lines(density(forest$W.hat[data$D == 1]), xlim = c(0, 1), col = "red")
+
+plot(density(nuisance$pscore_fit_lasso[data$D == 0]), xlim = c(0, 1))
+lines(density(nuisance$pscore_fit_lasso[data$D == 1]), xlim = c(0, 1))
+
+plot(density(nui$pscore_fit_rf[data$D == 0]), xlim = c(0, 1))
+lines(density(nui$pscore_fit_rf[data$D == 1]), xlim = c(0, 1))
