154f308fc1cf39e3506e24af4298c595ff978755
Bobby Ho <poke@bobbyho.me>
2023-05-28 15:31:12 +0800

    main: recursive mkdir

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ for (spec in c("both", "pscore", "resp")) {
         simulation_result <- with_progress(simulation_setup(500, 5, lrn_type, spec))
 
         output_dir <- "./results/tau5/"
-        if (!dir.exists(output_dir)) dir.create(output_dir)
+        if (!dir.exists(output_dir)) dir.create(output_dir, recursive = T)
 
         saveRDS(simulation_result, sprintf(paste0(output_dir, "sim_result_%s_%s.rds"), lrn_type, spec))
     }
@@ -22,7 +22,7 @@ for (spec in c("both", "pscore", "resp")) {
         simulation_result <- with_progress(simulation_setup(500, 0, lrn_type, spec))
 
         output_dir <- "./results/tau0/"
-        if (!dir.exists(output_dir)) dir.create(output_dir)
+        if (!dir.exists(output_dir)) dir.create(output_dir, recursive = T)
 
         saveRDS(simulation_result, sprintf(paste0(output_dir, "sim_result_%s_%s.rds"), lrn_type, spec))
     }
