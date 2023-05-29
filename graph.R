82a6bca4208c484c6de037d97624c8b3b8750d69
Bobby Ho <angela.bobby@gmail.com>
2023-05-29 16:14:41 +0800

    graph: update plotting functions

--- graph.R
+++ graph.R
@@ -1,30 +1,23 @@
 library(tidyverse)
+library(ggridges)
 
 simulation_result <- tibble()
 
 # Read results 
-for (spec in c("both", "pscore", "resp", "none")) {
+for (spec in c("both", "pscore", "resp")) {
     for (lrn_type in c("lasso", "rf", "xgboost")) {
-        result <- readRDS(sprintf("./results/sim_result_%s_%s.rds", lrn_type, spec))
+        result <- readRDS(sprintf("./results/tau0/sim_result_%s_%s.rds", lrn_type, spec))
         result$lrn_type <- lrn_type
         result$spec <- spec
         simulation_result <- bind_rows(simulation_result, result)
     }
 }
 
-sim_result_plot <- simulation_result %>%
+simulation_result %>%
     pivot_longer(starts_with("ate"), names_to = "estimator", values_to = "ate") %>%
-    ggplot(aes(x = ate, color = estimator)) +
+    ggplot(aes(x = ate, y = estimator)) +
         facet_grid(vars(spec), vars(lrn_type)) +
-        geom_line(stat = "density", alpha = 0.7) +
-        geom_point(
-            aes(y = 0),
-            data = . %>%
-                group_by(estimator) %>%
-                summarize(ate = mean(ate)),
-        ) +
-        geom_vline(xintercept = 5, linetype = "dashed") +
-        xlim(0, 10) +
+        geom_density_ridges(alpha = 0.7) +
+        geom_vline(xintercept = 0, linetype = "dashed") +
+        xlim(-5, 5) +
         theme_minimal()
-
-ggsave("./results/sim_result_plot.pdf", sim_result_plot, width = 10, height = 6, dpi = 300)
